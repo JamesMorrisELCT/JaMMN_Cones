8cf481f9f1e1e8aac91aa22dfd355af0115f0212
N8dogg72 <44811366+N8dogg72@users.noreply.github.com>
2021-11-30 15:24:11 -0500

    Max Power, Minimum Delay

---
+++ MaxPower/transmit_v2.ino
@@ -0,0 +1,235 @@
+#include <Wire.h>
+const int MPU = 0x68; // MPU6050 I2C address
+float Ax, Ay, Az;
+double MagA;
+double divider;
+float GyroX, GyroY, GyroZ;
+float accAngleX, accAngleY, gyroAngleX, gyroAngleY, gyroAngleZ;
+float roll, pitch, yaw;
+float AccErrorX, AccErrorY, GyroErrorX, GyroErrorY, GyroErrorZ;
+float elapsedTime, currentTime, previousTime;
+int c = 0;
+
+bool failure;
+unsigned long detectTime;
+unsigned long timeOn;
+
+#include <SPI.h>
+#include "printf.h"
+#include "RF24.h"
+
+// instantiate an object for the nRF24L01 transceiver
+RF24 radio(7, 8); // using pin 7 for the CE pin, and pin 8 for the CSN pin
+
+// Let these addresses be used for the pair
+uint8_t address[][6] = {"1Node", "2Node"};
+// It is very helpful to think of an address as a path instead of as
+// an identifying device destination
+// to use different addresses on a pair of radios, we need a variable to
+// uniquely identify which address this radio will use to transmit
+bool radioNumber = 1; // 0 uses address[0] to transmit, 1 uses address[1] to transmit
+
+// Used to control whether this node is sending or receiving
+bool role = true;  // true = TX role, false = RX role
+
+// For this example, we'll be using a payload containing
+// a single float number that will be incremented
+// on every successful transmission
+float payload = 0.0;
+
+void setup() {
+
+  Serial.begin(115200);
+  pinMode(2, OUTPUT);//LED
+  pinMode(3, INPUT); //SWITCH
+
+  // initialize the transceiver on the SPI bus
+  if (!radio.begin()) {
+    delay(10000);
+  }
+
+
+  radioNumber = 0;
+  // role variable is hardcoded to RX behavior, inform the user of this
+  //Serial.println(F("*** PRESS 'T' to begin transmitting to the other node"));
+
+  // Set the PA Level low to try preventing power supply related problems
+  // because these examples are likely run with nodes in close proximity to
+  // each other.
+  radio.setPALevel(RF24_PA_MAX);  // RF24_PA_MAX is default.
+
+  // save on transmission time by setting the radio to only transmit the
+  // number of bytes we need to transmit a float
+  radio.setPayloadSize(sizeof(payload)); // float datatype occupies 4 bytes
+
+  // set the TX address of the RX node into the TX pipe
+  radio.openWritingPipe(address[radioNumber]);     // always uses pipe 0
+
+  // set the RX address of the TX node into a RX pipe
+  radio.openReadingPipe(1, address[!radioNumber]); // using pipe 1
+
+  radio.stopListening();
+
+  // For debugging info
+  // printf_begin();             // needed only once for printing details
+  // radio.printDetails();       // (smaller) function that prints raw register values
+  // radio.printPrettyDetails(); // (larger) function that prints human readable data
+  setupIMU();
+
+  timeOn=2000; 
+  detectTime=millis()-timeOn;
+} // setup
+
+void loop() {
+  // This device is a TX node
+  double IMUsense=getIMU();
+  float switchIn = digitalRead(3);
+  float payload;
+  bool IMUdetect;
+  unsigned long currTime=millis();
+  if(currTime-detectTime<timeOn||IMUsense>4.0){
+    IMUdetect=true;
+    if(IMUsense>4.0){
+      detectTime=currTime;
+    }
+  } else {
+    IMUdetect=false;
+  }
+  if(switchIn==1.0||IMUdetect)
+  {
+    payload=1.0;
+  } else {
+    payload=0.0;
+  }
+  unsigned long start_timer = micros();                    // start the timer
+  bool report = radio.write(&payload, sizeof(float));      // transmit & save the report
+  unsigned long end_timer = micros();                      // end the timer
+
+
+  if (report) {
+    /*
+    Serial.print(F("Transmission successful! "));          // payload was delivered
+    Serial.print(F("Time to transmit = "));
+    Serial.print(end_timer - start_timer);                 // print the timer result
+    Serial.print(F(" us. Sent: "));
+    Serial.println(switchIn);                               // print payload sent
+    */
+    failure=false;
+
+  } else {
+    //Serial.println(F("Transmission failed or timed out")); // payload was not delivered
+    failure=true;
+  }
+
+
+  //Serial.print("Switch is: ");
+  //Serial.println(switchIn);
+  if(!failure){
+    digitalWrite(2, payload);
+  } else {
+    digitalWrite(2, HIGH);
+    delay(100);
+    digitalWrite(2,LOW);
+    delay(100);
+  }
+  // to make this example readable in the serial monitor
+  //delay(100);  // slow transmissions down by 0.1 second
+
+} // loop
+
+void calculate_IMU_error() {
+  // We can call this funtion in the setup section to calculate the accelerometer and gyro data error. From here we will get the error values used in the above equations printed on the Serial Monitor.
+  // Note that we should place the IMU flat in order to get the proper values, so that we then can the correct values
+  // Read accelerometer values 200 times
+  while (c < 200) {
+    Wire.beginTransmission(MPU);
+    Wire.write(0x3B);
+    Wire.endTransmission(false);
+    Wire.requestFrom(MPU, 6, true);
+    Ax = (Wire.read() << 8 | Wire.read()) / 16384.0 ;
+    Ay = (Wire.read() << 8 | Wire.read()) / 16384.0 ;
+    Az = (Wire.read() << 8 | Wire.read()) / 16384.0 ;
+    // Sum all readings
+    AccErrorX = AccErrorX + ((atan((Ay) / sqrt(pow((Ax), 2) + pow((Az), 2))) * 180 / PI));
+    AccErrorY = AccErrorY + ((atan(-1 * (Ax) / sqrt(pow((Ay), 2) + pow((Az), 2))) * 180 / PI));
+    c++;
+  }
+  //Divide the sum by 200 to get the error value
+  AccErrorX = AccErrorX / 200;
+  AccErrorY = AccErrorY / 200;
+  c = 0;
+  // Read gyro values 200 times
+  while (c < 200) {
+    Wire.beginTransmission(MPU);
+    Wire.write(0x43);
+    Wire.endTransmission(false);
+    Wire.requestFrom(MPU, 6, true);
+    GyroX = Wire.read() << 8 | Wire.read();
+    GyroY = Wire.read() << 8 | Wire.read();
+    GyroZ = Wire.read() << 8 | Wire.read();
+    // Sum all readings
+    GyroErrorX = GyroErrorX + (GyroX / 131.0);
+    GyroErrorY = GyroErrorY + (GyroY / 131.0);
+    GyroErrorZ = GyroErrorZ + (GyroZ / 131.0);
+    c++;
+  }
+  //Divide the sum by 200 to get the error value
+  GyroErrorX = GyroErrorX / 200;
+  GyroErrorY = GyroErrorY / 200;
+  GyroErrorZ = GyroErrorZ / 200;
+  // Print the error values on the Serial Monitor
+  Serial.print("AccErrorX: ");
+  Serial.println(AccErrorX);
+  Serial.print("AccErrorY: ");
+  Serial.println(AccErrorY);
+  Serial.print("GyroErrorX: ");
+  Serial.println(GyroErrorX);
+  Serial.print("GyroErrorY: ");
+  Serial.println(GyroErrorY);
+  Serial.print("GyroErrorZ: ");
+  Serial.println(GyroErrorZ);
+}
+
+void setupIMU() {
+  Wire.begin();                      // Initialize comunication
+  Wire.beginTransmission(MPU);       // Start communication with MPU6050 // MPU=0x68
+  Wire.write(0x6B);                  // Talk to the register 6B
+  Wire.write(0x00);                  // Make reset - place a 0 into the 6B register
+  Wire.endTransmission(true);        //end the transmission
+  divider = 16384.0;
+
+  // Configure Accelerometer Sensitivity - Full Scale Range (default +/- 2g)
+  Wire.beginTransmission(MPU);
+  Wire.write(0x1C);                  //Talk to the ACCEL_CONFIG register (1C hex)
+  Wire.write(0x10);                  //Set the register bits as 00010000 (+/- 8g full scale range)
+  Wire.endTransmission(true);
+  divider = 4096.0;
+  /*
+    // Configure Gyro Sensitivity - Full Scale Range (default +/- 250deg/s)
+    Wire.beginTransmission(MPU);
+    Wire.write(0x1B);                   // Talk to the GYRO_CONFIG register (1B hex)
+    Wire.write(0x10);                   // Set the register bits as 00010000 (1000deg/s full scale)
+    Wire.endTransmission(true);
+    delay(20);
+  */
+  // Call this function if you need to get the IMU error values for your module
+  calculate_IMU_error();
+
+  delay(20);
+}
+
+double getIMU() {
+// === Read acceleromter data === //
+  Wire.beginTransmission(MPU);
+  Wire.write(0x3B); // Start with register 0x3B (ACCEL_XOUT_H)
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU, 6, true); // Read 6 registers total, each axis value is stored in 2 registers
+  //For a range of +-2g, we need to divide the raw values by 16384, according to the datasheet
+  
+  Ax = (Wire.read() << 8 | Wire.read()) / divider; // X-axis value
+  Ay = (Wire.read() << 8 | Wire.read()) / divider; // Y-axis value
+  Az = (Wire.read() << 8 | Wire.read()) / divider; // Z-axis value
+
+  MagA=sqrt(Ax*Ax+Ay*Ay+Az*Az);
+  return MagA;
+}
